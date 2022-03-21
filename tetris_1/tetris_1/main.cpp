adf2905df72be600c791cc075061b20c87cf58c1
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2022-03-21 15:10:08 +0900

    FEAT: Background

--- tetris_1/tetris_1/main.cpp
+++ tetris_1/tetris_1/main.cpp
@@ -12,89 +12,170 @@ void gotoxy(int x, int y)
 	COORD pos = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+//===================================================================
+//-------------------------------------------------------------------
 
-unsigned char block_L[4][4] = { { 0, 0, 0, 0 },
-								{ 0, 1, 0, 0 },
-								{ 0, 1, 1, 1 },
-								{ 0, 0, 0, 0 } };
-
+void make_background();
+void print_background();
 void make_block();
 void delete_block();
+void insert_block();
 
-int x = 10;
-int y = 10;
+
 int count = 0;
+
+int x = 3;
+int y = 3;
+
+
+unsigned char background[22][12] = { { 1,1,1,1,1,1,1,1,1,1,1,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,0,0,0,0,0,0,0,0,0,0,1 },
+										{ 1,1,1,1,1,1,1,1,1,1,1,1 } };
+
+
+unsigned char block_L[4][4] = { { 0,0,0,0},
+								{ 0,1,0,0 },
+								{ 0,1,1,1 },
+								{ 0,0,0,0 } };
+
 
 void main()
 {
 
+
 	int ch = 0;
-	
-	
+
+	make_background();
+	print_background();
+
+	make_block();
+
 	while (1)
 	{
+		//----------------------------------
 		if (_kbhit())
 		{
 			ch = _getch();
+
 			if (ch == 'a')
 			{
-				gotoxy(x, y);
-				printf(" ");
-
+				delete_block();
 				x--;
-				gotoxy(x, y);
-				printf("*");
-			}
-			else if (ch == 'd')
-			{
-				gotoxy(x, y);
-				printf(" ");
+				make_block();
 
-				x++;
-				gotoxy(x, y);
-				printf("*");
 			}
 			else if (ch == 's')
 			{
-				gotoxy(x, y);
-				printf(" ");
-
+				delete_block();
 				y++;
-				gotoxy(x, y);
-				printf("*");
+				make_block();
+			}
+			else if (ch == 'd')
+			{
+				delete_block();
+				x++;
+				make_block();
 			}
 			else if (ch == 'w')
 			{
-				gotoxy(x, y);
-				printf(" ");
-
+				delete_block();
 				y--;
-				gotoxy(x, y);
-				printf("*");
+				make_block();
 			}
 		}
-		
-		if (count == 100)
+		//----------------------------------
+		if (count == 30)
+			//if(0)
 		{
 			count = 0;
 
-			/*gotoxy(x, y);
-			printf(" ");*/
-			delete_block();
+			if (y <= 17)
+			{
+				delete_block();
+				y++;
+				make_block();
+			}
+			else
+			{
+				insert_block();
+				print_background();
 
-			y++;
-
-			make_block();
-
-			/*gotoxy(x, y);
-			printf("*");*/
-
+				y = 3;
+			}
 		}
+
 		Sleep(10);
+
 		count++;
+
 	}
 
-	printf("Hello World");
+}
+
+
+
+int back_offset_x = 0;
+int back_offset_y = 0;
+
+void make_background()
+{
+	for (int j = 0; j < 22; j++)
+	{
+		for (int i = 0; i < 12; i++)
+		{
+			if (background[j][i] == 1)
+			{
+				gotoxy(back_offset_x + i, back_offset_y + j);
+				printf("*");
+			}
+			else
+			{
+				gotoxy(back_offset_x + i, back_offset_y + j);
+				printf("-");
+			}
+		}
+	}
+}
+
+
+
+void print_background()
+{
+	for (int j = 0; j < 22; j++)
+	{
+		for (int i = 0; i < 12; i++)
+		{
+			if (background[j][i] == 1)
+			{
+				gotoxy(back_offset_x + i + 15, back_offset_y + j);
+				printf("1");
+			}
+			else
+			{
+				gotoxy(back_offset_x + i + 15, back_offset_y + j);
+				printf("0");
+			}
+		}
+	}
 }
 
 
@@ -106,17 +187,19 @@ void make_block()
 		{
 			if (block_L[j][i] == 1)
 			{
-				gotoxy(x+i, y+j);
+				gotoxy(x + i, y + j);
 				printf("*");
 			}
 			else
 			{
-				gotoxy(x+i, y+j);
+				gotoxy(x + i, y + j);
 				printf("-");
 			}
 		}
 	}
 }
+
+
 
 void delete_block()
 {
@@ -126,13 +209,29 @@ void delete_block()
 		{
 			if (block_L[j][i] == 1)
 			{
-				gotoxy(x+i, y+j);
-				printf(" ");
+				gotoxy(x + i, y + j);
+				printf("-");
 			}
 			else
 			{
-				gotoxy(x+i, y+j);
-				printf(" ");
+				gotoxy(x + i, y + j);
+				printf("-");
+			}
+		}
+	}
+}
+
+
+
+void insert_block()
+{
+	for (int j = 0; j < 4; j++)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			if (block_L[j][i] == 1)
+			{
+				background[j + y][i + x] = 1;
 			}
 		}
 	}
